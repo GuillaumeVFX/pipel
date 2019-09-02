b2bda313bf01fa28521a68b5a0289fd0e2ce6d25
Guillaume Isabelle <jgi@jgwill.com>
2019-09-02 16:39:34 -0400

    simple script connecting to a remote machine and creating a sphere

--- clarisse/x_clarisse_remote_commanding_190902.py
+++ clarisse/x_clarisse_remote_commanding_190902.py
@@ -1,14 +1,6 @@
 #
 # status: Working :)
-MODULE_PATH = "./lib/clarisse_net.py"
-MODULE_NAME = "clarisse_net"
-import importlib
 import sys
-
-spec = importlib.util.spec_from_file_location(MODULE_NAME, MODULE_PATH)
-clarisse_net = importlib.util.module_from_spec(spec)
-sys.modules[spec.name] = clarisse_net
-spec.loader.exec_module(clarisse_net)
 import clarisse_net as ix
 
 rclarisse = ix.ClarisseNet('gaf',55000)
