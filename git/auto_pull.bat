a1bd726239e6887745396723c3aad5d61e88ce44
Jean Guillaume <jgi@jgwill.com>
2019-12-04 22:19:27 -0500

    houly

--- git/auto_pull.bat
+++ git/auto_pull.bat
@@ -2,5 +2,7 @@
 
 rem This will auto pull the repo and is destined to be run by windows task manager
 
-git pull
+git pull && ginol "NAD-Previz Repo was just pulled"
 
+
+cd blog && git pull && ginol "NAD-Previz WPApp Repo was just pulled"
